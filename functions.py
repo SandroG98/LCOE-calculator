17a6c532979cc0e8b52b4ec7c539f03fdcd601f4
Sandro <sandro.gmeiner@gmail.com>
2023-04-10 13:55:17 +0200

    Added empty function bodys

---
+++ functions.py
@@ -0,0 +1,17 @@
+def crf(interest, years):
+    return 0
+
+
+def fcost(invest, crf, T):
+    return 0
+
+
+def fuel(cost, hw=1, etha=1):
+    return 0
+
+
+def co2price(co2, emissionf=0, etha=1):
+    return 0
+
+def lcoe(fcost, vcost):
+    return 0
